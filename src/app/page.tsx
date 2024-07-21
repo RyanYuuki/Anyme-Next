934a2bb26f488ede1ee0aa23b29d5c3445542722
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-07-21 23:50:40 +0530

    - Added A Button for Anime Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex h-screen">
-      
+    <div className="flex justify-center items-center h-screen">
+      <Link href={'/Anime/'} >
+        <Button>Anime</Button>
+      </Link>
     </div>
   );
 }
---
+++ src/components/ReusableStack.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReusableStack = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default ReusableStack
