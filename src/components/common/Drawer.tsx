445b56ee3ae5b78c4c106547445a3ac93ef22498
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-08-18 21:52:46 +0530

    - Cleaning Up the Code

--- src/components/common/Drawer.tsx
+++ src/components/common/Drawer.tsx
@@ -1,19 +1,17 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faBook,
+  faClapperboard,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
@@ -34,20 +32,26 @@ export default function Drawer() {
           <SheetHeader>
             <h1 className="text-xl">AnYmeY</h1>
           </SheetHeader>
-          <div className="mt-10 flex flex-col items-center gap-7">
+          <div className="mt-10 flex flex-col items-left gap-7">
             <SheetClose asChild>
               <Link href={`/`}>
-                <h1>Home</h1>
+                <span>
+                  <FontAwesomeIcon className="mr-2" icon={faHome} /> Home
+                </span>
               </Link>
             </SheetClose>
             <SheetClose asChild>
               <Link href={`/pages/Anime/`}>
-                <h1>Anime</h1>
+                <span>
+                  <FontAwesomeIcon className="mr-2" icon={faClapperboard} /> Anime
+                </span>
               </Link>
             </SheetClose>
             <SheetClose asChild>
               <Link href={`/pages/Manga/`}>
-                <h1>Manga</h1>
+                <span>
+                  <FontAwesomeIcon className="mr-2" icon={faBook} /> Manga
+                </span>
               </Link>
             </SheetClose>
           </div>
